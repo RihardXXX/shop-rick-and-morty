e56b0b4fbe24af1e6c3e2b1e689d9ac0d1a73805
RihardXXX <yusupa.akaeva@yandex.ru>
2021-08-30 13:22:11 +0300

    completed project

--- src/store.js
+++ src/store.js
@@ -1,7 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+// Чтобы экшены можно бфло вкладывать и в виде чисто строк а не объектов
+const setupStringAction = (store) => (next) => (action) => {
+  if (typeof action === 'string') {
+    console.log('метка');
+    return next({
+      type: action,
+    });
+  }
+
+  return next(action);
+};
+
+const store = createStore(reducer, applyMiddleware(setupStringAction));
+
+store.dispatch('start');
 
 export default store;
